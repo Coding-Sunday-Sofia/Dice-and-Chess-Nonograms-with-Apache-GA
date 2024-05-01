b84f32686bfefd9bf4276e6226e03cf11199983b
Todor Balabanov <todor.balabanov@gmail.com>
2024-05-01 14:36:12 +0300

    Optimization process has been improved.

--- app/src/main/java/eu/veldsoft/chess/nonogram/App.java
+++ app/src/main/java/eu/veldsoft/chess/nonogram/App.java
@@ -1,34 +1,30 @@
 package eu.veldsoft.chess.nonogram;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 import org.apache.commons.math3.exception.MathIllegalArgumentException;
 import org.apache.commons.math3.genetics.AbstractListChromosome;
 import org.apache.commons.math3.genetics.Chromosome;
+import org.apache.commons.math3.genetics.ElitisticListPopulation;
+import org.apache.commons.math3.genetics.FixedGenerationCount;
 import org.apache.commons.math3.genetics.GeneticAlgorithm;
 import org.apache.commons.math3.genetics.InvalidRepresentationException;
 import org.apache.commons.math3.genetics.MutationPolicy;
 import org.apache.commons.math3.genetics.TournamentSelection;
 import org.apache.commons.math3.genetics.UniformCrossover;
-import org.apache.commons.math3.genetics.FixedGenerationCount;
-import org.apache.commons.math3.genetics.ElitisticListPopulation;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
-import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 
 public class App {
-	private static final int GENERATIONS = 10000;
+	private static final int GENERATIONS = 100000;
 	private static final int POPULATION = 51;
 
 	private static enum Cell {
-		EMPTY(' '), KING('K'), QUEEN('Q'), ROOK('R'), BISHOP('B'), KNIGHT('N');
+		EMPTY(' '), OCCUPIED('*'), KING('K'), QUEEN('Q'), ROOK('R'), BISHOP('B'), KNIGHT('N');
 
 		static class Step {
 			int dx;
@@ -48,7 +44,7 @@ public class App {
 
 		private char symbol;
 
-		private List<Step> steps = new ArrayList<>();
+		private List<List<Step>> steps = new ArrayList<>();
 
 		Cell(char symbol) {
 			this.symbol = symbol;
@@ -58,28 +54,20 @@ public class App {
 			return symbol;
 		}
 
-		void steps(List<Step> steps) {
+		void steps(List<List<Step>> steps) {
 			this.steps = steps;
 		}
 
-		List<Step> steps() {
+		List<List<Step>> steps() {
 			return steps;
 		}
 	}
 
 	private static final Random PRNG = new Random();
 
-	private static final Set<Cell> PIECES = new HashSet<>();
+	private static final Cell PIECES[] = { Cell.KING, Cell.QUEEN, Cell.ROOK, Cell.BISHOP, Cell.KNIGHT, };
 
 	private static int[][] image = { {} };
-
-	static {
-		PIECES.add(Cell.KING);
-		PIECES.add(Cell.QUEEN);
-		PIECES.add(Cell.ROOK);
-		PIECES.add(Cell.BISHOP);
-		PIECES.add(Cell.KNIGHT);
-	}
 
 	private static Cell[][] board(List<Cell> cells, int[][] image) {
 		Cell[][] board = new Cell[image.length][];
@@ -104,27 +92,37 @@ public class App {
 		}
 
 		Cell[][] board = board(cells, image);
+		List<Cell> pieces = Arrays.asList(PIECES);
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
 				if (board[i][j] == Cell.EMPTY) {
 					continue;
 				}
+				if (board[i][j] == Cell.OCCUPIED) {
+					continue;
+				}
 
-				for (Cell.Step step : board[i][j].steps()) {
-					if (i + step.dx < 0) {
-						continue;
-					}
-					if (i + step.dx >= counters.length) {
-						continue;
-					}
-					if (j + step.dy < 0) {
-						continue;
-					}
-					if (j + step.dy >= counters[i].length) {
-						continue;
-					}
+				for (List<Cell.Step> directions : board[i][j].steps()) {
+					for (Cell.Step step : directions) {
+						if (i + step.dx < 0) {
+							continue;
+						}
+						if (i + step.dx >= counters.length) {
+							continue;
+						}
+						if (j + step.dy < 0) {
+							continue;
+						}
+						if (j + step.dy >= counters[i].length) {
+							continue;
+						}
 
-					counters[i + step.dx][j + step.dy]++;
+						if (pieces.contains(board[i + step.dx][j + step.dy])) {
+							break;
+						}
+
+						counters[i + step.dx][j + step.dy]++;
+					}
 				}
 			}
 		}
@@ -140,29 +138,18 @@ public class App {
 		Cell[][] board = board(cells, image);
 
 		int score = 0;
-		final int BAD = -20;
-		final int GOOD = +200;
-		final int OVERLAP = -10;
-		final int OVERBEATEN = -10;
+		final int BAD = -10;
+		final int GOOD = +100;
 		final int UNDERBEATEN = -5;
 		for (int i = 0; i < counters.length; i++) {
 			for (int j = 0; j < counters[i].length; j++) {
-//				if (counters[i][j] == 2 && image[i][j] == 1) {
-//					score += GOOD;
-//				}
-//				if (counters[i][j] > 2 && image[i][j] == 1) {
-//					score += OVERBEATEN;
-//				}
-				if (counters[i][j] >= 2 && image[i][j] == 1) {
+				if (counters[i][j] >= 2 && board[i][j] == Cell.OCCUPIED) {
 					score += GOOD;
 				}
-				if (counters[i][j] < 2 && image[i][j] == 1) {
+				if (counters[i][j] < 2 && board[i][j] == Cell.OCCUPIED) {
 					score += UNDERBEATEN;
 				}
-				if (board[i][j] != Cell.EMPTY && image[i][j] == 1) {
-					score += OVERLAP;
-				}
-				if (counters[i][j] > 0 && image[i][j] == 0) {
+				if (counters[i][j] > 1 && image[i][j] == 0) {
 					score += BAD;
 				}
 			}
@@ -177,11 +164,11 @@ public class App {
 		for (int i = 0; i < image.length; i++) {
 			for (int j = 0; j < image[i].length; j++) {
 				if (image[i][j] == 1) {
-					cells.add(Cell.EMPTY);
+					cells.add(Cell.OCCUPIED);
 				} else if (PRNG.nextDouble() < (1D - threshold)) {
 					cells.add(Cell.EMPTY);
 				} else {
-					cells.add(Cell.values()[PRNG.nextInt(Cell.values().length)]);
+					cells.add(PIECES[PRNG.nextInt(PIECES.length)]);
 				}
 			}
 		}
@@ -193,9 +180,8 @@ public class App {
 		int[][] counters = beaten(cells, image);
 		for (int i = 0, k = 0; i < image.length; i++) {
 			for (int j = 0; j < image[i].length; j++, k++) {
-				System.out.print(
-						"[" + cells.get(k).symbol() + "" + (image[i][j] == 1 ? '*' : (counters[i][j] > 0 ? ' ' : ' '))
-								+ (counters[i][j] > 0 ? String.format("%2d", counters[i][j]) : "  ") + "]");
+				System.out.print("[" + cells.get(k).symbol() + ""
+						+ (counters[i][j] > 1 ? String.format("%2d", counters[i][j]) : "  ") + "]");
 			}
 			System.out.println();
 		}
@@ -244,6 +230,9 @@ public class App {
 	}
 
 	public static void main(String[] args) throws IOException {
+//		args = new String[] { "C:\\Users\\Todor Balabanov\\Desktop\\1.txt",
+//				"C:\\Users\\Todor Balabanov\\Desktop\\2.txt" };
+
 		List<int[]> rows = new ArrayList<>();
 		try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
 			String line;
@@ -264,67 +253,145 @@ public class App {
 			}
 		}
 
-		int[][] board = dice(image);
-		try (PrintStream out = new PrintStream(new FileOutputStream(args[1]))) {
-			out.println(Arrays.deepToString(board).replace("],", "\n").replace("[", "").replace("]", "")
-					.replace(" ", "").replace(",", ""));
-			out.close();
-		}
-		System.exit(0);
+//		int[][] board = dice(image);
+//		try (PrintStream out = new PrintStream(new FileOutputStream(args[1]))) {
+//			out.println(Arrays.deepToString(board).replace("],", "\n").replace("[", "").replace("]", "")
+//					.replace(" ", "").replace(",", ""));
+//			out.close();
+//		}
+//		System.exit(0);
 
-		List<Cell.Step> steps = null;
+		List<Cell.Step> direction = null;
+		List<List<Cell.Step>> steps = null;
 
-		steps = new ArrayList<Cell.Step>();
-		steps.add(new Cell.Step(-1, -1));
-		steps.add(new Cell.Step(-1, 0));
-		steps.add(new Cell.Step(-1, 1));
-		steps.add(new Cell.Step(0, -1));
-		steps.add(new Cell.Step(0, 1));
-		steps.add(new Cell.Step(1, -1));
-		steps.add(new Cell.Step(1, 0));
-		steps.add(new Cell.Step(1, 1));
+		steps = new ArrayList<>();
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(-1, -1));
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(-1, 0));
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(-1, 1));
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(0, -1));
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(0, 1));
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(1, -1));
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(1, 0));
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(1, 1));
+		steps.add(direction);
+
 		Cell.KING.steps(steps);
 
-		steps = new ArrayList<Cell.Step>();
-		steps.add(new Cell.Step(-2, -1));
-		steps.add(new Cell.Step(-2, 1));
-		steps.add(new Cell.Step(-1, -2));
-		steps.add(new Cell.Step(-1, 2));
-		steps.add(new Cell.Step(1, -2));
-		steps.add(new Cell.Step(1, 2));
-		steps.add(new Cell.Step(2, -1));
-		steps.add(new Cell.Step(2, 1));
+		steps = new ArrayList<>();
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(-2, -1));
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(-2, 1));
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(-1, -2));
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(-1, 2));
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(1, -2));
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(1, 2));
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(2, -1));
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		direction.add(new Cell.Step(2, 1));
+		steps.add(direction);
+
 		Cell.KNIGHT.steps(steps);
 
-		steps = new ArrayList<Cell.Step>();
+		steps = new ArrayList<>();
+
+		direction = new ArrayList<>();
 		for (int i = 1; i < size; i++) {
-			steps.add(new Cell.Step(-i, -i));
-			steps.add(new Cell.Step(-i, i));
-			steps.add(new Cell.Step(i, -i));
-			steps.add(new Cell.Step(i, i));
+			direction.add(new Cell.Step(-i, -i));
 		}
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		for (int i = 1; i < size; i++) {
+			direction.add(new Cell.Step(-i, i));
+		}
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		for (int i = 1; i < size; i++) {
+			direction.add(new Cell.Step(i, -i));
+		}
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		for (int i = 1; i < size; i++) {
+			direction.add(new Cell.Step(i, i));
+		}
+		steps.add(direction);
+
 		Cell.BISHOP.steps(steps);
+		Cell.QUEEN.steps(steps);
 
-		steps = new ArrayList<Cell.Step>();
+		steps = new ArrayList<>();
+
+		direction = new ArrayList<>();
 		for (int i = 1; i < size; i++) {
-			steps.add(new Cell.Step(-i, 0));
-			steps.add(new Cell.Step(0, -i));
-			steps.add(new Cell.Step(0, i));
-			steps.add(new Cell.Step(i, 0));
+			direction.add(new Cell.Step(-i, 0));
 		}
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		for (int i = 1; i < size; i++) {
+			direction.add(new Cell.Step(0, -i));
+		}
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		for (int i = 1; i < size; i++) {
+			direction.add(new Cell.Step(0, i));
+		}
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		for (int i = 1; i < size; i++) {
+			direction.add(new Cell.Step(i, 0));
+		}
+		steps.add(direction);
+
 		Cell.ROOK.steps(steps);
-
-		steps = new ArrayList<Cell.Step>();
-		for (int i = 1; i < size; i++) {
-			steps.add(new Cell.Step(-i, -i));
-			steps.add(new Cell.Step(-i, i));
-			steps.add(new Cell.Step(i, -i));
-			steps.add(new Cell.Step(i, i));
-			steps.add(new Cell.Step(-i, 0));
-			steps.add(new Cell.Step(0, -i));
-			steps.add(new Cell.Step(0, i));
-			steps.add(new Cell.Step(i, 0));
-		}
 		Cell.QUEEN.steps(steps);
 
 		class CellChromosome extends AbstractListChromosome<Cell> {
@@ -343,7 +410,7 @@ public class App {
 			@Override
 			public CellChromosome newFixedLengthChromosome(List<Cell> representation) {
 				representation = new ArrayList<>(representation);
-				Collections.shuffle(representation);
+				// Collections.shuffle(representation);
 				return new CellChromosome(representation);
 			}
 
@@ -358,8 +425,15 @@ public class App {
 			public Chromosome mutate(Chromosome original) throws MathIllegalArgumentException {
 				List<Cell> representation = new ArrayList<>(((CellChromosome) original).representation());
 
-				representation.set(PRNG.nextInt(representation.size()),
-						Cell.values()[PRNG.nextInt(Cell.values().length)]);
+				int index = PRNG.nextInt(representation.size());
+				if (representation.get(index) != Cell.OCCUPIED) {
+					if (PRNG.nextDouble() < 0.5D) {
+						Cell cell = PIECES[PRNG.nextInt(PIECES.length)];
+						representation.set(index, cell);
+					} else {
+						representation.set(index, Cell.EMPTY);
+					}
+				}
 
 				return new CellChromosome(representation);
 			}
@@ -367,7 +441,8 @@ public class App {
 
 		List<Chromosome> chromosomes = new ArrayList<Chromosome>();
 		for (int i = 0; i < POPULATION; i++) {
-			chromosomes.add(new CellChromosome(random(image, 0.3)));
+			List<Cell> representation = random(image, 0.3);
+			chromosomes.add(new CellChromosome(representation));
 		}
 
 		Chromosome fittest = ga.evolve(new ElitisticListPopulation(chromosomes, chromosomes.size() * 2, 0.05),
