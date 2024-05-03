d91bb669c2a5454a9ff8f61377e840a65824ad8e
Todor Balabanov <todor.balabanov@gmail.com>
2024-05-03 19:22:43 +0300

    Some bugs have been fixed.

--- app/src/main/java/eu/veldsoft/chess/nonogram/App.java
+++ app/src/main/java/eu/veldsoft/chess/nonogram/App.java
@@ -1,8 +1,10 @@
 package eu.veldsoft.chess.nonogram;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -143,10 +145,10 @@ public class App {
 		Cell[][] board = board(representation, image);
 
 		int score = 0;
-		final int BAD = -300;
+		final int BAD = -150;
 		final int GOOD = +50;
 		final int BEST = +100;
-		final int UNDERBEATEN = -10;
+		final int UNDERBEATEN = +10;
 		for (int i = 0; i < counters.length; i++) {
 			for (int j = 0; j < counters[i].length; j++) {
 				if (counters[i][j] == 2 && board[i][j] == Cell.OCCUPIED) {
@@ -192,6 +194,18 @@ public class App {
 		return false;
 	}
 
+	private static boolean hitting(int index, Cell cell, int[][] image2) {
+		for (int i = 0, k = 0; i < image.length; i++) {
+			for (int j = 0; j < image[i].length; j++, k++) {
+				if (index == k) {
+					return hitting(i, j, cell, image);
+				}
+			}
+		}
+
+		return false;
+	}
+
 	private static List<Cell> random(int[][] image, double threshold) {
 		List<Cell> representation = new ArrayList<>();
 
@@ -201,7 +215,7 @@ public class App {
 					representation.add(Cell.OCCUPIED);
 				} else {
 					Cell piece = PIECES_ARRAY[PRNG.nextInt(PIECES_ARRAY.length)];
-					if (hitting(i, j, piece, image) && PRNG.nextDouble() < threshold) {
+					if (PRNG.nextDouble() < threshold) {
 						representation.add(piece);
 					} else {
 						representation.add(Cell.EMPTY);
@@ -231,19 +245,14 @@ public class App {
 	}
 
 	private static List<Cell> removeHarmful(List<Cell> representation, int[][] image) {
-		List<Cell> result = new ArrayList<>();
-		int[][] counters = beaten(representation, image);
+		List<Cell> result = new ArrayList<>(representation);
 
+		int[][] counters = beaten(result, image);
 		for (int i = 0, k = 0; i < image.length; i++) {
 			for (int j = 0; j < image[i].length; j++, k++) {
-				if (PIECES_LIST.contains(representation.get(k)) == false) {
-					result.add(representation.get(k));
-					continue;
-				}
-
 				boolean isHarmful = false;
 
-				done: for (List<Cell.Step> directions : representation.get(k).steps()) {
+				done: for (List<Cell.Step> directions : result.get(k).steps()) {
 					for (Cell.Step step : directions) {
 						if (i + step.dx < 0) {
 							continue;
@@ -265,10 +274,12 @@ public class App {
 					}
 				}
 
-				if (isHarmful == false) {
-					result.add(representation.get(k));
-				} else {
-					result.add(Cell.EMPTY);
+				if (isHarmful) {
+					result.set(k, Cell.EMPTY);
+					counters = beaten(result, image);
+					i = 0;
+					j = 0;
+					k = 0;
 				}
 			}
 		}
@@ -276,18 +287,18 @@ public class App {
 		return result;
 	}
 
-	private static void print(boolean debug, List<Cell> representation, int[][] image) {
+	private static void print(PrintStream out, boolean debug, List<Cell> representation, int[][] image) {
 		int[][] counters = beaten(representation, image);
 		for (int i = 0, k = 0; i < image.length; i++) {
 			for (int j = 0; j < image[i].length; j++, k++) {
 				if (debug == true) {
-					System.out.print("[" + representation.get(k).symbol() + ""
+					out.print("[" + representation.get(k).symbol() + ""
 							+ (counters[i][j] > 1 ? String.format("%2d", counters[i][j]) : "  ") + "]");
 				} else {
-					System.out.print(representation.get(k).symbol());
+					out.print(representation.get(k).symbol());
 				}
 			}
-			System.out.println();
+			out.println();
 		}
 	}
 
@@ -333,10 +344,54 @@ public class App {
 		return board;
 	}
 
+	private static int unbeaten(List<Cell> representation, int[][] image) {
+		int[][] counters = beaten(representation, image);
+
+		int sum = 0;
+		for (int i = 0; i < image.length; i++) {
+			for (int j = 0; j < image[i].length; j++) {
+				if (image[i][j] == 1 && counters[i][j] < 2) {
+					sum++;
+				}
+			}
+		}
+
+		return sum;
+	}
+
+	private static List<Cell> chess(int[][] image) {
+		List<Cell> representation = new ArrayList<>();
+
+		for (int i = 0; i < image.length; i++) {
+			for (int j = 0; j < image[i].length; j++) {
+				if (image[i][j] == 1) {
+					representation.add(Cell.OCCUPIED);
+				} else {
+					representation.add(Cell.EMPTY);
+				}
+			}
+		}
+
+		do {
+			int index = -1;
+			do {
+				index = PRNG.nextInt(representation.size());
+			} while (representation.get(index) != Cell.EMPTY);
+
+			Cell cell = PIECES_ARRAY[PRNG.nextInt(PIECES_ARRAY.length)];
+
+			representation.set(index, cell);
+			representation = removeUnused(representation, image);
+			representation = removeHarmful(representation, image);
+		} while (unbeaten(representation, image) > 0);
+
+		return representation;
+	}
+
 	public static void main(String[] args) throws IOException {
 //		args = new String[] { "C:\\Users\\Todor Balabanov\\Desktop\\Icons-32x32-03-May-2024\\01.bin",
-//				"C:\\Users\\Todor Balabanov\\Desktop\\Icons-32x32-03-May-2024\\01.txt" };
-//
+//				"C:\\Users\\Todor Balabanov\\Desktop\\Icons-32x32-03-May-2024\\01.chess" };
+
 		List<int[]> rows = new ArrayList<>();
 		try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
 			String line;
@@ -357,13 +412,13 @@ public class App {
 			}
 		}
 
-//		int[][] board = dice(image);
-//		try (PrintStream out = new PrintStream(new FileOutputStream(args[1]))) {
-//			out.println(Arrays.deepToString(board).replace("],", "\n").replace("[", "").replace("]", "")
-//					.replace(" ", "").replace(",", ""));
-//			out.close();
-//		}
-//		System.exit(0);
+		/*
+		 * int[][] board = dice(image); try (PrintStream out = new PrintStream(new
+		 * FileOutputStream(args[1]))) {
+		 * out.println(Arrays.deepToString(board).replace("],", "\n").replace("[",
+		 * "").replace("]", "") .replace(" ", "").replace(",", "")); out.close(); }
+		 * System.exit(0); /
+		 */
 
 		List<Cell.Step> direction = null;
 		List<List<Cell.Step>> steps = null;
@@ -467,7 +522,6 @@ public class App {
 		steps.add(direction);
 
 		Cell.BISHOP.steps(steps);
-		Cell.QUEEN.steps(steps);
 
 		steps = new ArrayList<>();
 
@@ -496,7 +550,65 @@ public class App {
 		steps.add(direction);
 
 		Cell.ROOK.steps(steps);
+
+		steps = new ArrayList<>();
+
+		direction = new ArrayList<>();
+		for (int i = 1; i < size; i++) {
+			direction.add(new Cell.Step(-i, -i));
+		}
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		for (int i = 1; i < size; i++) {
+			direction.add(new Cell.Step(-i, i));
+		}
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		for (int i = 1; i < size; i++) {
+			direction.add(new Cell.Step(i, -i));
+		}
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		for (int i = 1; i < size; i++) {
+			direction.add(new Cell.Step(i, i));
+		}
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		for (int i = 1; i < size; i++) {
+			direction.add(new Cell.Step(-i, 0));
+		}
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		for (int i = 1; i < size; i++) {
+			direction.add(new Cell.Step(0, -i));
+		}
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		for (int i = 1; i < size; i++) {
+			direction.add(new Cell.Step(0, i));
+		}
+		steps.add(direction);
+
+		direction = new ArrayList<>();
+		for (int i = 1; i < size; i++) {
+			direction.add(new Cell.Step(i, 0));
+		}
+		steps.add(direction);
+
 		Cell.QUEEN.steps(steps);
+
+		/*
+		 * List<Cell> board = chess(image); print(System.out, true, board, image);
+		 * Cell.EMPTY.symbol('.'); Cell.OCCUPIED.symbol('.'); try (PrintStream out = new
+		 * PrintStream(new FileOutputStream(args[1]))) { print(out, false, board,
+		 * image); out.close(); } System.exit(0); /
+		 */
 
 		class CellChromosome extends AbstractListChromosome<Cell> {
 			@Override
@@ -544,7 +656,7 @@ public class App {
 
 		List<Chromosome> chromosomes = new ArrayList<Chromosome>();
 		for (int i = 0; i < POPULATION; i++) {
-			List<Cell> representation = removeHarmful(removeUnused(random(image, 0.01), image), image);
+			List<Cell> representation = random(image, 0.09);
 			chromosomes.add(new CellChromosome(representation));
 		}
 
@@ -553,10 +665,14 @@ public class App {
 
 		List<Cell> representation = ((CellChromosome) fittest).representation();
 		representation = removeUnused(representation, image);
+		representation = removeHarmful(representation, image);
 		System.out.println(fitness(representation, image));
-		print(true, representation, image);
+		print(System.out, true, representation, image);
 		Cell.EMPTY.symbol('.');
 		Cell.OCCUPIED.symbol('.');
-		print(false, representation, image);
+		try (PrintStream out = new PrintStream(new FileOutputStream(args[1]))) {
+			print(out, false, representation, image);
+			out.close();
+		}
 	}
 }
